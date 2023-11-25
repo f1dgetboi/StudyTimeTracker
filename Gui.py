566f75036a4d533f0a2350c86e36d21183994574
f1dgetboi <116887514+f1dgetboi@users.noreply.github.com>
2023-11-25 09:01:09 +0900

    initial commit

---
+++ Gui.py
@@ -0,0 +1,267 @@
+import pygame, sys
+from pygame.locals import *
+import cv2
+#initialise
+pygame.init()
+monitor_size = [pygame.display.Info().current_w, pygame.display.Info().current_h]
+mainClock = pygame.time.Clock()
+DEFAULT_FONT = r"Zen_Old_Mincho\ZenOldMincho-Medium.ttf"
+
+#ウィンドウの作成
+
+def create_window(width, height,background_colour,caption):
+    global screen, window_colour,WIDTH,HEIGHT,display
+    WIDTH = width
+    HEIGHT = height
+    window_colour = background_colour
+    display = pygame.display.set_mode((WIDTH, HEIGHT), pygame.HWSURFACE | pygame.DOUBLEBUF |pygame.FULLSCREEN)
+    screen = pygame.Surface((width, height))
+    pygame.display.set_caption(str(caption))
+    #screen.fill(background_colour)
+    pygame.display.flip()
+
+#テキストの描画
+def create_text(font,fontsize,color,x,y,text):
+    font = pygame.font.Font(font, fontsize)
+    text = font.render(text, True, color)
+    screen.blit(text, (x,y))
+
+def create_center_text(font,fontsize,color,x,y,text):
+    font = pygame.font.Font(font, fontsize)
+    text = font.render(text, True, color)
+    text_rect = text.get_rect(center=(x, y))
+    screen.blit(text, text_rect)
+
+#OPENCVのIMGをPYGAMEのIMGに転換
+
+def BGR_TO_RGB(img):
+    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    height, width, _ = rgb_img.shape
+    # Create a Pygame surface from the pixel buffer
+    pygame_image = pygame.image.frombuffer(rgb_img.flatten(), (width, height), 'RGB')
+
+    return pygame_image
+
+#写真の大きさを変える関数
+
+def resize_image(image, width, height):
+    resized_image = pygame.transform.scale(image, (width, height))
+    return resized_image
+
+def create_drop_shadow(rect, size):
+    shadow_surface = pygame.Surface((rect.width + size, rect.height + size ))
+    shadow_surface.set_alpha(2)
+    for i in range(-5, 4):
+        for j in range(-5, 4):
+            screen.blit(shadow_surface, (rect.x + i * size/3 , rect.y + j * size/3))
+        
+
+#他はオブジェクトのコード
+class GuiObject:
+    def __init__(self,w,h,x,y,color,texture=None,visible=True,stroke=False,stroke_width=5,stroke_color=(255,255,255),rounded=False,roundness=3,hollow=False,background=False,Shadow=False,shadow_size=None,):
+        self.w = w
+        self.h = h
+        self.x = x
+        self.y = y
+        self.color = color
+        self.original_color = color
+        self.visible = visible
+        self.stroke = stroke
+        self.stroke_width = stroke_width
+        self.stroke_color = stroke_color
+        self.rounded = rounded
+        self.roundness = roundness
+        self.hollow = hollow
+        if texture is not None:
+            self.texture = pygame.image.load(texture)
+            #self.texture = resize_image(self.texture,self.w,self.h)
+            print("working")
+        else:
+            self.texture = None
+        self.clicked_color = (self.color[0]* 1.2,self.color[1]* 1.2,self.color[2]* 1.2)
+        self.hoverd_color = (self.color[0]* 0.8,self.color[1]* 0.8,self.color[2]* 0.8)
+        self.background = background
+        self.shadow = Shadow
+        self.rect = pygame.Rect((self.x,self.y),(self.w, self.h))
+        self.shadow_size = shadow_size
+        
+    def draw(self):
+        if self.visible == True:
+            if self.shadow:
+                create_drop_shadow(self.rect, self.shadow_size)  
+            if self.rounded:   
+                if self.stroke:
+
+                    pygame.draw.rect(screen, self.stroke_color,((self.x - self.stroke_width,self.y - self.stroke_width),(self.w + self.stroke_width *2, self.h + self.stroke_width *2)),0,self.roundness)
+                    
+                if self.texture is not None:
+                    if self.background:                   
+                        pygame.draw.rect(screen, self.color,((self.x,self.y),(self.w, self.h)),0,self.roundness)
+                    screen.blit(self.texture,(self.x,self.y))    
+                elif self.texture is None:
+                    pygame.draw.rect(screen, self.color,((self.x,self.y),(self.w, self.h)),0,self.roundness)    
+                
+            else:
+
+                if self.stroke:
+                    pygame.draw.rect(screen, self.stroke_color,((self.x - self.stroke_width,self.y - self.stroke_width),(self.w + self.stroke_width *2, self.h + self.stroke_width *2)),int(self.hollow))
+                if self.texture is not None:
+                    if self.background:
+                        pygame.draw.rect(screen, self.color,((self.x,self.y),(self.w, self.h)),int(self.hollow))
+                    screen.blit(self.texture,(self.x,self.y))
+                elif self.texture is None:
+                    pygame.draw.rect(screen, self.color,((self.x,self.y),(self.w, self.h)),int(self.hollow))
+
+class Button(GuiObject):
+    def __init__(self,w,h,x,y,color,texture=None,visible=True,stroke=True,stroke_width=5,stroke_color=(255,255,255),rounded=False,roundness=3,hollow=False,background=False,text=None,fontsize=None,text_offset_x=None,text_offset_y=None,Shadow=False,shadow_size=None):
+        super().__init__(w, h, x, y, color, texture, visible, stroke, stroke_width, stroke_color, rounded, roundness, hollow, background,Shadow,shadow_size)
+        self.hovered = False
+        self.clicked = False
+        self.rect = pygame.Rect((self.x,self.y),(self.w, self.h))
+        self.text = text
+        self.fontsize = fontsize
+        self.text_offset_x = text_offset_x
+        self.text_offset_y = text_offset_y
+        self.changed = False
+    def draw(self):
+        super().draw()
+        if self.text is not None:
+            create_text(DEFAULT_FONT,self.fontsize,(0,0,0),self.x + self.text_offset_x,self.y + self.text_offset_y,self.text)
+
+    def check_clicks(self,value=None,how_much=None,bool=False,string=False,list=False):
+        pos = pygame.mouse.get_pos()
+
+        self.color = self.original_color
+        if self.rect.collidepoint(pygame.mouse.get_pos()): 
+
+            self.color = self.hoverd_color
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND) 
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True                
+                if value is not None:
+                    if not bool:
+                        if string:
+                            value = how_much
+                        else:
+                            if list:
+                                value.append(how_much)
+                            else:                            
+                                value =  value + how_much
+                    else:
+                        if value == True and self.changed == False:
+                            value = False
+                            self.changed = True
+                        if value == False and self.changed == False:
+                            value = True
+            
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+            self.changed = False
+        return value
+
+            
+                            
+class TextBox():
+    def __init__(self,w,h,x,y,color,text,fontsize,text_offset_x=0,text_offset_y=0,visible=True,stroke=False,stroke_width=5,stroke_color=(255,255,255),rounded=False,roundness=3,hollow=False):
+        self.w = w
+        self.h = h
+        self.x = x
+        self.y = y
+        self.color = color
+        self.original_color = color
+        self.visible = visible
+        self.stroke = stroke
+        self.stroke_width = stroke_width
+        self.stroke_color = stroke_color
+        self.rounded = rounded
+        self.roundness = roundness
+        self.hollow = hollow
+        self.text = text
+        self.fontsize = fontsize
+        self.text_offset_x = text_offset_x
+        self.text_offset_y = text_offset_y
+
+    def draw(self):
+
+        if self.visible == True:
+
+            if self.rounded:   
+
+                if self.stroke:
+                    pygame.draw.rect(screen, self.stroke_color,((self.x - self.stroke_width,self.y - self.stroke_width),(self.w + self.stroke_width *2, self.h + self.stroke_width *2)),0,self.roundness)
+                pygame.draw.rect(screen, self.color,((self.x,self.y),(self.w, self.h)),0,self.roundness)    
+                create_text(DEFAULT_FONT,self.fontsize,(0,0,0),self.x + self.text_offset_x,self.y + self.text_offset_y,self.text)
+            else:
+
+                if self.stroke:
+                    pygame.draw.rect(screen, self.stroke_color,((self.x - self.stroke_width,self.y - self.stroke_width),(self.w + self.stroke_width *2, self.h + self.stroke_width *2)),int(self.hollow))
+                pygame.draw.rect(screen, self.color,((self.x,self.y),(self.w, self.h)),int(self.hollow))
+                create_text(DEFAULT_FONT,self.fontsize,(0,0,0),self.x + self.text_offset_x,self.y + self.text_offset_y,self.text)
+
+class buy_frame():
+    def __init__(self,x,y,color,price,name,photo): #photo size is going to be 375 x 300
+        self.name= name
+        self.width = 400
+        self.height = 400
+        self.x = x
+        self.y = y
+        self.color = color
+        self.price = price
+        self.name = name
+        self.photo = self.texture = pygame.image.load(photo)
+        self.background = Button(self.width,self.height,self.x,self.y,self.color)
+        self.background.hoverd_color = self.color
+    def draw(self):
+        self.background.shadow = False
+        if self.background.rect.collidepoint(pygame.mouse.get_pos()): 
+            self.background.shadow = True
+            self.background.shadow_size = 10
+        self.background.rounded = True
+        self.background.roundness = 10
+        self.background.draw()
+        screen.blit(self.photo,(self.x + 13 ,self.y + 10))
+        create_center_text(DEFAULT_FONT,40,(0,0,0),self.x + self.width /2  ,self.y + self.height- 60 ,self.name)
+        create_center_text(DEFAULT_FONT,30,(100,100,100),self.x + self.width /2 ,self.y + self.height- 17 ,self.price)
+
+COLOR_ACTIVE = (10,10,10)
+COLOR_INACTIVE = (100,100,100)
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface =  pygame.font.Font(DEFAULT_FONT,32).render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+
+            if self.rect.collidepoint(event.pos):
+                self.active = not self.active
+            else:
+                self.active = False
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                self.txt_surface =  pygame.font.Font(DEFAULT_FONT,32).render(self.text, True, self.color)
+
+    def update(self):
+        if self.rect.collidepoint(pygame.mouse.get_pos()):
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_IBEAM)      
+        width = max(150, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255,255,255), self.rect, 0)
+        pygame.draw.rect(screen, self.color, self.rect, 5)
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y-5))
+
---
+++ main.py
@@ -0,0 +1,193 @@
+import Gui
+import pygame, sys 
+from pygame import *
+import time
+from datetime import date,timedelta
+
+Gui.create_window(Gui.monitor_size[0],Gui.monitor_size[1],(255,255,255),"Time keeper")
+shakai_time = 0
+mainClock = pygame.time.Clock()
+rika_time = 0
+kokugo_time = 0
+suugaku_time = 0
+today = date.today()
+weekday = today.isoweekday()
+print(weekday)
+week_study_time = [0,0,0,0]
+BUTTON_COLORS = (225, 217, 209)
+BUTTON_DIMENSIONS = (300,50)
+LEFT = (Gui.monitor_size[0] /4 -380, Gui.monitor_size[1] /2 -250) 
+MIDDLE_LEFT = (Gui.monitor_size[0] /4 *2 -400, Gui.monitor_size[1] /2 +300)
+MIDDLE_RIGHT = (Gui.monitor_size[0] /4 *3 -400, Gui.monitor_size[1] /2)
+RIGHT = (Gui.monitor_size[0] /4 *4 -400, Gui.monitor_size[1] /2)
+shakai_button = Gui.Button(BUTTON_DIMENSIONS[0],BUTTON_DIMENSIONS[1],LEFT[0],MIDDLE_LEFT[1],BUTTON_COLORS,text="shakai start",fontsize=50,text_offset_x=20,text_offset_y=-20)
+rika_button = Gui.Button(BUTTON_DIMENSIONS[0],BUTTON_DIMENSIONS[1],MIDDLE_LEFT[0],MIDDLE_LEFT[1],BUTTON_COLORS,text="rika start",fontsize=50,text_offset_x=50,text_offset_y=-20)
+kokugo_button = Gui.Button(BUTTON_DIMENSIONS[0],BUTTON_DIMENSIONS[1],MIDDLE_RIGHT[0],MIDDLE_LEFT[1],BUTTON_COLORS,text="kokugo start",fontsize=50,text_offset_x=20,text_offset_y=-20)
+suugaku_button = Gui.Button(BUTTON_DIMENSIONS[0],BUTTON_DIMENSIONS[1],RIGHT[0],MIDDLE_LEFT[1],BUTTON_COLORS,text="suugaku start",fontsize=50,text_offset_x=10,text_offset_y=-20)
+end_button = Gui.Button(BUTTON_DIMENSIONS[0],BUTTON_DIMENSIONS[1],MIDDLE_LEFT[0] + 100,MIDDLE_LEFT[1]+100,BUTTON_COLORS,text="End",fontsize=50,text_offset_x=150,text_offset_y=-20)
+current_context =  "select menu"
+timer_type = None
+start = 0
+
+def convert_string(num):
+    hours = 0
+    minutes = 0
+    seconds = 0
+    if num >= 60:
+        if num >= 3600:
+            hours = num // 3600
+        minutes = (num - (hours * 3600))// 60
+    seconds = num - (hours * 3600) - (minutes * 60)
+    return f"{hours}:{minutes}:{seconds}"
+
+def get_study_time(subject):
+        week_time = 0
+        today_time = 0
+        for i in range(weekday):
+            day = today - timedelta(days=i)
+            try:
+                with open(f'{day}.txt', 'r') as f:
+                    content = f.readlines()
+                    if day == today:
+                        if subject == "shakai":
+                            today_time += int(content[1])
+                        if subject == "rika":
+                            today_time += int(content[2])   
+                        if subject == "kokugo":
+                            today_time += int(content[3])   
+                        if subject == "suugaku":
+                            today_time += int(content[4]) 
+                    if subject == "shakai":
+                        week_time += int(content[1])
+                    if subject == "rika":
+                        week_time += int(content[2])   
+                    if subject == "kokugo":
+                        week_time += int(content[3])   
+                    if subject == "suugaku":
+                        week_time += int(content[4])        
+                print(day)
+            except:
+                print("No data")
+        return week_time,today_time
+
+
+week_study_time[0] = get_study_time("shakai")[0]
+shakai_time = get_study_time("shakai")[1]
+week_study_time[1] = get_study_time("rika")[0]
+rika_time = get_study_time("rika")[1]
+week_study_time[2] = get_study_time("kokugo")[0]
+kokugo_time = get_study_time("kokugo")[1]
+week_study_time[3] = get_study_time("suugaku")[0]
+suugaku_time = get_study_time("suugaku")[1]
+
+def extract_data(start_date,end_date):
+    try:
+        start_date = date(2008, 8, 15) 
+        end_date = date(2008, 9, 15)    # perhaps date.now()
+
+        delta = end_date - start_date   # returns timedelta
+
+        for i in range(delta.days + 1):
+            day = start_date + timedelta(days=i)
+            print(day)
+    except:
+        print("No viable data")
+
+def event_listen():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            with open(f'{today}.txt', 'w') as f:
+                #f.write(f'Y is: {testsubject.y} X is: {testsubject.x} width is: {testsubject.w} height is: {testsubject.h}')
+                f.write(f'shakai:{shakai_time}, rika:{rika_time}, kokugo:{kokugo_time}, suugaku:{suugaku_time}\n{shakai_time}\n{rika_time}\n{kokugo_time}\n{suugaku_time}')
+            pygame.quit()
+            sys.exit()
+
+    surf = pygame.transform.scale(Gui.screen,(Gui.monitor_size))
+    Gui.display.blit(surf,(0,0))
+    pygame.display.update()
+        
+def drawtoScreen():
+    global current_context, shakai_time,rika_time,kokugo_time,suugaku_time,start,timer_type
+    Gui.screen.fill((255,255,255)) 
+    if current_context == "select menu":
+        shakai_frame = Gui.buy_frame(LEFT[0], LEFT[1], BUTTON_COLORS, f"shakai day: {convert_string(shakai_time)}", f"shakai week: {convert_string(week_study_time[0])}", "image.png")
+        rika_frame = Gui.buy_frame(MIDDLE_LEFT[0], LEFT[1], BUTTON_COLORS, f"rika day: {convert_string(rika_time)}", f"rika week: {convert_string(week_study_time[1])}", "image.png")
+        kokugo_frame = Gui.buy_frame(MIDDLE_RIGHT[0], LEFT[1], BUTTON_COLORS, f"kokugo day: {convert_string(kokugo_time)}", f"kokugo week: {convert_string(week_study_time[2])}", "image.png")
+        suugaku_frame = Gui.buy_frame(RIGHT[0], LEFT[1], BUTTON_COLORS, f"suugaku day: {convert_string(suugaku_time)}", f"suugaku week: {convert_string(week_study_time[3])}", "image.png")
+        shakai_frame.draw()
+        rika_frame.draw()
+        kokugo_frame.draw()
+        suugaku_frame.draw()
+        shakai_button.draw()
+        rika_button.draw()
+        kokugo_button.draw()
+        suugaku_button.draw()
+        shakai_button.check_clicks()
+        rika_button.check_clicks()
+        kokugo_button.check_clicks()
+        suugaku_button.check_clicks()  
+        if rika_button.clicked:
+            timer_type = "rika"
+            current_context = "timer_screen"
+            rika_button.clicked = False
+            start = time.time()
+        if shakai_button.clicked:
+            timer_type = "shakai"
+            current_context = "timer_screen"
+            start = time.time()
+            shakai_button.clicked = False
+        if kokugo_button.clicked:
+            timer_type = "kokugo"
+            current_context = "timer_screen"
+            start = time.time()
+            kokugo_button.clicked = False
+        if suugaku_button.clicked:
+            timer_type = "suugaku"
+            current_context = "timer_screen"
+            start = time.time()
+            suugaku_button.clicked = False
+    if current_context == "timer_screen":
+        Gui.screen.fill((255,255,255)) 
+        end_button.draw()
+        end_button.check_clicks()
+        current = time.time()
+        sum_time = int(current - start)
+        print(sum_time)
+        if timer_type == "rika":
+            Gui.create_center_text(Gui.DEFAULT_FONT,100,(0,0,0),Gui.monitor_size[0]/2,Gui.monitor_size[1]/2 ,f"Day time:{convert_string(rika_time + sum_time)}")
+            Gui.create_center_text(Gui.DEFAULT_FONT,100,(0,0,0),Gui.monitor_size[0]/2,Gui.monitor_size[1]/2 +100,f"week time:{convert_string(week_study_time[1] + sum_time)}")
+        if timer_type == "shakai":
+            Gui.create_center_text(Gui.DEFAULT_FONT,100,(0,0,0),Gui.monitor_size[0]/2,Gui.monitor_size[1]/2 ,f"Day time:{convert_string(shakai_time + sum_time)}")
+            Gui.create_center_text(Gui.DEFAULT_FONT,100,(0,0,0),Gui.monitor_size[0]/2,Gui.monitor_size[1]/2 +100,f"week time:{convert_string(week_study_time[0] + sum_time)}")
+        if timer_type == "suugaku":
+            Gui.create_center_text(Gui.DEFAULT_FONT,100,(0,0,0),Gui.monitor_size[0]/2,Gui.monitor_size[1]/2 ,f"Day time:{convert_string(suugaku_time + sum_time)}")
+            Gui.create_center_text(Gui.DEFAULT_FONT,100,(0,0,0),Gui.monitor_size[0]/2,Gui.monitor_size[1]/2 +100,f"week time:{convert_string(week_study_time[3] + sum_time)}")
+        if timer_type == "kokugo":
+            Gui.create_center_text(Gui.DEFAULT_FONT,100,(0,0,0),Gui.monitor_size[0]/2,Gui.monitor_size[1]/2 ,f"Day time:{convert_string(kokugo_time + sum_time)}")
+            Gui.create_center_text(Gui.DEFAULT_FONT,100,(0,0,0),Gui.monitor_size[0]/2,Gui.monitor_size[1]/2 +100,f"week time:{convert_string(week_study_time[2] + sum_time)}")
+        Gui.create_center_text(Gui.DEFAULT_FONT,100,(0,0,0),Gui.monitor_size[0]/2,Gui.monitor_size[1]/2 -100,f"study session:{convert_string(sum_time)}")
+
+        if end_button.clicked == True:
+            if timer_type == "rika":
+                rika_time += sum_time
+                week_study_time[1] += sum_time
+            if timer_type == "shakai":
+                shakai_time += sum_time
+                week_study_time[0] += sum_time
+            if timer_type == "suugaku":
+                suugaku_time += sum_time
+                week_study_time[3] += sum_time
+            if timer_type == "kokugo":
+                kokugo_time += sum_time
+                week_study_time[2] += sum_time
+            current_context = "select menu"
+            end_button.clicked = False
+        
+
+
+def main():
+    while True:
+        drawtoScreen()
+        event_listen()
+        mainClock.tick(30)
+main()
